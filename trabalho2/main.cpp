e2e31473e8170400e3b385073c543ce620c75f83
Alon Mota <Alon Mota>
2017-04-25 18:48:52 -0300

    basico para abertura de arquivos e recebimento de parametros

--- trabalho2/main.cpp
+++ trabalho2/main.cpp
@@ -1,8 +1,34 @@
 #include <stdio.h>
-#include <string.h>
+
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+#include <string>
+using std::string;
+using std::getline;
+
+#define MEM_SIZE 4096
 
 
-int main(){
+
+
+
+
+int32_t mem[MEM_SIZE];
+
+int main(int argc, char *argv[]){
+
+	char *codigo	= (argv[1]);
+	char *dados	= (argv[2]);
 	
+	FILE *fcode = fopen(codigo, "rb");
+	FILE *fdata = fopen(dados, "rb");
+
+
+
+	fclose(fcode);
+	fclose(fdata);
 	return 0;
 }
