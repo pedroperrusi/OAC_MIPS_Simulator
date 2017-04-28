7c5a9107ccfb424ae2d7dffc673a254cb575c198
Perruci <pedro.perruci@gmail.com>
2017-04-28 16:21:29 -0300

    moved readBinaryFile to src/

---
+++ trabalho2/src/readBinaryFile.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+
+#include <iostream>
+#include <fstream>
+#include <iterator>
+#include <bitset>
+
+int loadBinFile( std::string, char** );
+
+int main( int argc, char* argv[] ) {
+
+	if( argc == 1 )
+		return 0;
+
+	char * memblock;
+
+	int size;
+
+	size = loadBinFile( argv[1], &memblock );
+
+	std::cout << "Size: " << size << '\n';
+
+	delete[] memblock;
+
+	return 0;
+}
+
+int loadBinFile( std::string fileName, char ** memblock )
+{
+	std::ifstream myfile; // File stream for reading
+	myfile.open ( fileName,  std::ios::in|std::ios::binary|std::ios::ate );
+
+	std::streampos size;
+
+	if( myfile.is_open() )
+	{
+		size = myfile.tellg();
+    	*memblock = new char [size];
+    	myfile.seekg (0, std::ios::beg);
+    	myfile.read (*memblock, size);
+    	myfile.close();
+    	std::cout << "File content loaded in the memory\n";
+	}
+	else
+	{
+		std::cout << "Error when opening" << '\n';
+		return 0;
+	}
+	return size;
+}
