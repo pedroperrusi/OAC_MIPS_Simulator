ed742254f69b7de9288a122650f4dc43470ec749
Perruci <pedro.perruci@gmail.com>
2017-04-28 17:31:55 -0300

    Implemented a loop for reading binary values on file

--- trabalho2/src/readBinaryFile.cpp
+++ trabalho2/src/readBinaryFile.cpp
@@ -6,46 +6,68 @@
 #include <iterator>
 #include <bitset>
 
-int loadBinFile( std::string, char** );
+#define MEM_SIZE 4096
+int32_t mem[MEM_SIZE];
+
+int loadBinFile( std::string );
+
+void dump_mem(/* int start int end char format */);
 
 int main( int argc, char* argv[] ) {
 
 	if( argc == 1 )
 		return 0;
 
-	char * memblock;
-
+//	char * memblock;
 	int size;
 
-	size = loadBinFile( argv[1], &memblock );
+	size = loadBinFile( argv[1] );
 
 	std::cout << "Size: " << size << '\n';
 
-	delete[] memblock;
+	//dump_mem();
+
+//	delete[] memblock;
 
 	return 0;
 }
 
-int loadBinFile( std::string fileName, char ** memblock )
+int loadBinFile( std::string fileName )
 {
 	std::ifstream myfile; // File stream for reading
-	myfile.open ( fileName,  std::ios::in|std::ios::binary|std::ios::ate );
+	myfile.open ( fileName,  std::ios::in|std::ios::binary );
+
+	uint32_t a;
 
 	std::streampos size;
 
 	if( myfile.is_open() )
 	{
+		// Move to the end of the file
+		myfile.seekg( 0, std::ios::end );
+		// Get file size
 		size = myfile.tellg();
-    	*memblock = new char [size];
-    	myfile.seekg (0, std::ios::beg);
-    	myfile.read (*memblock, size);
-    	myfile.close();
-    	std::cout << "File content loaded in the memory\n";
+		// Return to beginning
+		myfile.seekg( 0, std::ios::beg );
+		std::cout << "Memory" << '\n';
+		while( myfile.tellg() < size )
+		{
+			myfile.read(reinterpret_cast<char *>(&a), sizeof(a));
+			std::cout << std::bitset<32>(myfile.tellg()) << "\t = \t" << std::bitset<32>(a) << '\n';
+			getchar();
+		}
+		myfile.close();
 	}
 	else
 	{
-		std::cout << "Error when opening" << '\n';
+		std::cout << "Error when opening file" << '\n';
 		return 0;
 	}
 	return size;
 }
+
+// void dump_mem(/* int start int end char format */)
+// {
+// 	std::bitset<MEM_SIZE> bits((std::string(memblock)));
+// 	std::cout << "Raw memory Loaded:\n"<< bits << '\n';
+// }
