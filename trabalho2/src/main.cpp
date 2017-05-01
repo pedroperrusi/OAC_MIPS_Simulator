f49482073c7996b59e71afd167599f46430445ed
Perruci <pedro.perruci@gmail.com>
2017-05-01 01:04:05 -0300

    Avoid MIPS_simulator as pointer

--- trabalho2/src/main.cpp
+++ trabalho2/src/main.cpp
@@ -2,15 +2,14 @@
 #include "core.hpp"
 #include "MIPS_simulador.hpp"
 
-MIPS_simulador* simulador_;
+int main(int argc, char *argv[])
+{
+	MIPS_simulador simulador( argv[1], argv[2] );
 
-int main(int argc, char *argv[]){
-
-	simulador_ = new MIPS_simulador( argv[1], argv[2] );
 	std::cout << ".text" << '\n';
-	simulador_ -> mips_ -> dump_mem( MEM_TEXT_BEGIN, MEM_TEXT_END, 'h' );
+	simulador.mips_ -> dump_mem( MEM_TEXT_BEGIN, MEM_TEXT_END, 'h' );
 	std::cout << ".data" << '\n';
-	simulador_ -> mips_ -> dump_mem( MEM_DATA_BEGIN, MEM_DATA_END, 'h' );
+	simulador.mips_ -> dump_mem( MEM_DATA_BEGIN, MEM_DATA_END, 'h' );
 
 
 	return 0;
